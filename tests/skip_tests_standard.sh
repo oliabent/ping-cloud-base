46c230cdb2396acdf117200dd821ce5e3531c702
Walker <walkerbrown@pingidentity.com>
2023-08-09 13:31:34 -0600

    Removing unused files, injecting SKIP_TEST var back in integration base

--- tests/skip_tests_standard.sh
+++
@@ -1,4 +0,0 @@
-# This file will be sourced during CI/CD execution, and appended to any BRANCH_SKIP_TESTS variables that have been previously declared.
-# It is intended for 'standard' (non customer-hub) deploys
-export SCHEDULE_SKIP_TESTS="pingcentral/prerequisites/00-test-urls.sh
-pingcentral/prerequisites/01-verify-pods.sh"
